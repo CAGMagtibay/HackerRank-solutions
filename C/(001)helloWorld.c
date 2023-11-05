9592712675e6979736c108f92ec8d06e623c43ca
Christopher Magtibay <cagmagtibay@live.com>
2023-11-05 18:34:52 -0500

    Create (001)helloWorld.c

---
+++ C/(001)helloWorld.c
@@ -0,0 +1,25 @@
+/*
+TASK:
+  FUNCTION DESCRIPTION:
+    Complete the main() function below.
+    The main() function has the following input:
+      string s: a string
+  PRINTS:
+    *two strings: * "Hello, World!" on one line and the input string on the next line.
+INPUT FORMAT:
+  There is one line of text, s.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
