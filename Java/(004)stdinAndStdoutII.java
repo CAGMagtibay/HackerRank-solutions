05b259b13b1aff18123af20b199eaa464e016770
Christopher Magtibay <cagmagtibay@live.com>
2023-11-06 18:42:00 -0500

    Create (004)stdinAndStdoutII.java

---
+++ Java/(004)stdinAndStdoutII.java
@@ -0,0 +1,32 @@
+/*
+TASK:
+  Read an integer, a double, and a String from stdin, then print the values according to the instructions.
+INPUT FORMAT:
+  There are three lines of input:
+    The first line contains an integer.
+    The second line contains a double.
+    The third line contains a String.
+OUTPUT FORMAT:
+  There are three lines of output:
+    On the first line, print String: followed by the unaltered String read from stdin.
+    On the second line, print Double: followed by the unaltered double read from stdin.
+    On the third line, print Int: followed by the unaltered integer read from stdin.
+*/
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
