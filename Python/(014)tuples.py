f6e1113d58a8e63009781b4a4b06fd8660388675
Christopher Magtibay <cagmagtibay@live.com>
2023-11-13 18:35:48 -0500

    Create (014)tuples.py

---
+++ Python/(014)tuples.py
@@ -0,0 +1,16 @@
+'''
+TASK:
+  Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. 
+  Then compute and print the result of hash(t).
+INPUT FORMAT:
+  The first line contains an integer, n, denoting the number of elements in the tuple.
+  The second line contains n space-separated integers describing the elements in tuple t.
+OUTPUT FORMAT:
+  Print the result of hash(t).
+'''
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    
+    t = tuple(integer_list)
+    print(hash(t))
