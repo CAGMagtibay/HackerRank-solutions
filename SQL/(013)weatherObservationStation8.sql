20e6439b836bcd6f83aa85b751029163e11db0be
Christopher Magtibay <cagmagtibay@live.com>
2023-11-13 16:49:17 -0500

    Create (013)weatherObservationStation8.sql

---
+++ SQL/(013)weatherObservationStation8.sql
@@ -0,0 +1,4 @@
+/*
+TASK: Query the list of CITY names from STATION which have vowels as both their first and last characters. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u') AND RIGHT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u');
