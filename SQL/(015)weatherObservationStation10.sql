4214d20c6d48b37b5250d0de4c1aeb0c73c9e78b
Christopher Magtibay <cagmagtibay@live.com>
2023-11-14 15:44:40 -0500

    Create (015)weatherObservationStation10.sql

---
+++ SQL/(015)weatherObservationStation10.sql
@@ -0,0 +1,4 @@
+/*
+TASK: Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
